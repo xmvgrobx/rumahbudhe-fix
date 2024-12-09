6ca67863d3d467b46c486f91298ff67a29285818
xmvgrobx <akunrereempat@gmail.com>
2024-12-10 05:04:55 +0700

    ++please bisa++

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,18 @@
-import { authOptions } from "@/lib/auth"
-import { getServerSession } from "next-auth"
-import Lists from "@/components/Lists"
-
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
+import Lists from "@/components/Lists";
 
 export default async function Home() {
-  const session = await getServerSession(authOptions)
+  const session = await getServerSession(authOptions);
 
   return (
     <div className="grid justify-center items-center h-[80vh]">
       <div>
+        {/* Ensure Lists is used correctly */}
         <Lists />
-      
-        <pre>{JSON.stringify(session)}</pre>
+
+        {/* <pre>{JSON.stringify(session)}</pre> */}
       </div>
     </div>
-  )
+  );
 }
-
-  // import { authOptions } from "@/lib/auth"
-  // import { getServerSession } from "next-auth"
-  // import Lists from "@/components/Lists"
-  // export default async function Home() {
-  //   const session = await getServerSession(authOptions)
-
-  //   return (
-  //     <div className="grid justify-center items-center h-[80vh]">
-  //       <div>
-  //         <Lists />
-
-  //         <pre>{JSON.stringify(session)}</pre>
-  //       </div>
-  //     </div>
-  //   )
-  // }
